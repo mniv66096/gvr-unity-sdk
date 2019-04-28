20917efe01fe4aab1cf832ee4cdc4128203c41b3
Siyuan.Q <siyuan.qiu1@gmail.com>
2019-04-28 18:57:34 -0400

    Changed cursor size

--- Assets/GoogleVR/Scripts/Cardboard/GvrReticlePointer.cs
+++ Assets/GoogleVR/Scripts/Cardboard/GvrReticlePointer.cs
@@ -28,10 +28,10 @@ public class GvrReticlePointer : GvrBasePointer
     public const float RETICLE_MIN_INNER_ANGLE = 0.0f;
 
     /// Minimum outer angle of the reticle (in degrees).
-    public const float RETICLE_MIN_OUTER_ANGLE = 0.5f;
+    public const float RETICLE_MIN_OUTER_ANGLE = 0.1f;
 
     /// Angle at which to expand the reticle when intersecting with an object (in degrees).
-    public const float RETICLE_GROWTH_ANGLE = 1.5f;
+    public const float RETICLE_GROWTH_ANGLE = 0.3f;
 
     /// Minimum distance of the reticle (in meters).
     public const float RETICLE_DISTANCE_MIN = 0.45f;
